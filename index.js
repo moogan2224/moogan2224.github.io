e60653ebe9c508af51157c530304e910c11cb51b
moogan2224 <80492417+moogan2224@users.noreply.github.com>
2023-11-13 20:11:41 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,50 +1,15 @@
-const color = 300;
-const color2 = "300";
-let secondColor = "pink";
-console.log("secondColor: ", secondColor);
+// get slideshow area from galleryImg container
+const galleryContainer = document.querySelector('.galleryImg');
+const forwardButton = document.querySelector('[data-js="forwardButton"]');
+const backButton = document.querySelector('[data-js="backButton"]');
 
-secondColor = "blue";
-console.log("after resassign secondColor: ", secondColor);
+// console.log(galleryImg);
 
-console.log(document);
+if(galleryContainer){
+	$(galleryContainer).slick({
+		slidesToShow: 1,
+		prevArrow:$(backButton),
+		nextArrow:$(forwardButton),
+	});
 
-const section = document.querySelectorAll(".section");
-console.log(section);
-
-const li1 = document.getElementById("listItem1");
-li1.innerHTML = "Whatever we want";
-
-const sections = document.getElementsByClassName("section");
-
-sections[0].style.backgroundColor = "red";
-
-sections[0].onclick = function () {
-	console.log("inside the click handler");
-};
-
-sections[0].onmouseleave = function () {
-	sections[0].style.backgroundColor = "green";
-};
-
-document.addEventListener(
-	"keydown",
-	function (event) {
-		console.log(event);
-		const name = event.key;
-		// console.log("key pressed: ", name);
-		// var code = event.code;
-		if (name === "Escape") {
-			sections[0].style.backgroundColor = "pink";
-		}
-	},
-	false
-);
-
-const newElement = document.createElement("blockquote");
-
-const newParagraph = document.createElement("p");
-
-newParagraph.innerHTML = "Hello all -Wade";
-
-newElement.appendChild(newParagraph);
-document.body.appendChild(newElement);
+}
