34dc409b6b03f6c59467583645ce7fe351dfec62
moogan2224 <80492417+moogan2224@users.noreply.github.com>
2023-11-20 17:52:36 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // get slideshow area from galleryImg container
 const galleryContainer = document.querySelector('.galleryImg');
+const galleryContainerSmall = document.querySelector('.galleryGrid');
+console.log(galleryContainerSmall);
 const forwardButton = document.querySelector('[data-js="forwardButton"]');
 const backButton = document.querySelector('[data-js="backButton"]');
 
@@ -10,6 +12,13 @@ if(galleryContainer){
 		slidesToShow: 1,
 		prevArrow:$(backButton),
 		nextArrow:$(forwardButton),
+		asNavFor: galleryContainerSmall 
+	});	
+	$(galleryContainerSmall).slick({
+		slidesToShow: 1,
+		asNavFor: galleryContainer,
+		dots: true,
+		focusOnSelect: true,
 	});
 
 }
